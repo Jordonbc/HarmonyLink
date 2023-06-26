74a89995474b85a89d1e5a02b3c644e529f2f1d6
Jordon Brooks <jordonbc@hotmail.co.uk>
2023-06-26 18:32:43 +0100

    Fixed compile error on Linux

--- src/v1/docking/stats.rs
+++ src/v1/docking/stats.rs
@@ -1,7 +1,7 @@
 use std::{io::BufReader, fs::File};
 
 #[allow(unused_imports)]
-use std::collections::hash_set;
+use std::collections::HashSet;
 
 use crate::{v1::{battery::{stats::get_battery_info, structs::ChargingStatus}}, USE_FALLBACK_DOCK_DETECTION};
 
